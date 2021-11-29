3d598fbbb43e1aa05b6822a8ee497d16ad6e723c
paggiLuca <paggi.luca@iisgalvanimi.edu.it>
2021-11-29 10:01:04 +0000

    using array

--- angular-hello-world/src/app/user-item/user-item.component.ts
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name:string;
-  constructor() { 
-    this.name = "Ciao"
-  }
+  @Input() name:string 
+  constructor() { }
   ngOnInit(): void {
   }
 
